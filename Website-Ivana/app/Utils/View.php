0eda15ea64875402941e168872d69e791da8fc24
RobsDuarte <robsduar@gmail.com>
2021-06-28 16:50:31 -0300

    add guardian if

--- Website-Ivana/app/Utils/View.php
+++ Website-Ivana/app/Utils/View.php
@@ -8,11 +8,16 @@ class View {
     }
 
     public static function render($view , $vars = []){
+        
+        if(empty($vars)) return self::getContentView($view);
+
         $keys = array_keys($vars);
         $keys = array_map(function($element_in_keys){
             return '{{'.$element_in_keys.'}}';
         },$keys);             
+
         $content = self::getContentView($view);
+
         return str_replace($keys,array_values($vars) ,$content);
     }
 }
