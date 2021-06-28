6ff9d49c7b3dfcb20efa78da5841fd2f4655f14e
RobsDuarte <robsduar@gmail.com>
2021-06-28 16:51:54 -0300

    extends Template

--- Website-Ivana/app/Controller/home.php
+++ Website-Ivana/app/Controller/home.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Utils\View;
-
-class Home{    
-    public static function gethome(){
+class Home extends Template{    
+    public static function getHome() {
         $home_content = __DIR__.'/../../View/home.html';
         $home_content = file_get_contents($home_content);
-        return View::render('template',['content' => $home_content]);
+        $content =  View::render('home');
+        return self::getTemplate($content);
     }    
 }
