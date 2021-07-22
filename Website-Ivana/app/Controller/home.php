9a98360d8adf0fc09bdf534b430981afb4f13c9e
RobsDuarte <robsduar@gmail.com>
2021-07-22 17:09:55 -0300

    2 lines deleted bc wasnt necessary

--- Website-Ivana/app/Controller/home.php
+++ Website-Ivana/app/Controller/home.php
@@ -4,9 +4,7 @@ namespace App\Controller;
 
 use App\Utils\View;
 class Home extends Template{    
-    public static function getHome() {
-        $home_content = __DIR__.'/../../View/home.html';
-        $home_content = file_get_contents($home_content);
+    public static function getHome() {        
         $content =  View::render('home');
         return self::getTemplate($content);
     }    
