c83f11cba7c7a0df48446abca3731249d26f846f
RobsDuarte <robsduar@gmail.com>
2021-09-04 15:06:35 -0300

    init instanciada

--- Website-Ivana/index.php
+++ Website-Ivana/index.php
@@ -3,11 +3,20 @@
 require __DIR__."/vendor/autoload.php";
 
 use App\Http\Router;
+use App\Utils\View;
 
 define('URL','http://localhost/projetos/Website-Ivana');
 
+//Define o valor padrão das variaveis
+View::init([
+    'URL' =>  URL
+]);
+
+//incia o router
 $obRouter = new Router(URL);
 
+// inclue as rotas das páginas
 include __DIR__."/App/Routes/Pages.php";
 
+//imprime a resposta do servidor
 $obRouter->run()->sendResponse();
