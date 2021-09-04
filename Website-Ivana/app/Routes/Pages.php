53e7b7975e67fd2f450fd901ecc3e819f2238846
RobsDuarte <robsduar@gmail.com>
2021-09-04 15:06:13 -0300

    rotas modificadas

--- Website-Ivana/app/Routes/Pages.php
+++ Website-Ivana/app/Routes/Pages.php
@@ -12,21 +12,21 @@ $obRouter ->get('/',[
     }
 ]);
 
-$obRouter ->get('/professores',[
+$obRouter ->get('/Professores',[
     function()
     {
         return new Response(200,Teacher::getTeacher());
     }
 ]);
 
-$obRouter ->get('/empresas',[
+$obRouter ->get('/Empresas',[
     function()
     {
         return new Response(200,Empresa::getEmpresa());
     }
 ]);
 
-$obRouter ->get('/alunos',[
+$obRouter ->get('/Alunos',[
     function()
     {
         return new Response(200,Home::getHome());
