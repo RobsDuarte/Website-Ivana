271dd18edca11f09e99fcf3606f104a662a5fb48
RobsDuarte <robsduar@gmail.com>
2021-09-04 15:06:00 -0300

    func init criada

--- Website-Ivana/app/Utils/View.php
+++ Website-Ivana/app/Utils/View.php
@@ -2,13 +2,25 @@
 
 namespace App\Utils;
 class View {
+
+    private static $vars = [];
+    /**
+     * Método resposável por definir os padrões iniciais da classe 
+     *
+     * @param array $vars
+     */
+    public static function init($vars = []){
+        self::$vars = $vars;
+    }
+
     private static function getContentView($view){
         $file = __DIR__.'/../../View/'.$view.'.html';
         return file_exists($file) ? file_get_contents($file) : "";
     }
 
     public static function render($view , $vars = []){
-        
+        $vars = array_merge(self::$vars,$vars);
+    
         if(empty($vars)) return self::getContentView($view);
 
         $keys = array_keys($vars);
